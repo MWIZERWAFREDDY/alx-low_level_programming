253ee7809e91e93a0455f002e61ff5be9cf64eb8
FREDDY MWIZERWA <57982668+MWIZERWAFREDDY@users.noreply.github.com>
2022-03-23 22:12:36 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * function that copies a string
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
-char *_strncpy(char *dest, char *sr
+int _strcmp(char *s1, char *s2)
 {
-	char c = "dest";
-        char d = "src";
-        char *dest;
-                dest = _strcmp(c, d);
-        return (dest);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
