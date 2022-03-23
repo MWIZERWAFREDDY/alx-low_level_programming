b30be20c4cff12ac926f902bf702e7d4a9c5cec7
MWIZERWAFREDDY <mwizerwafreddy@gmail.com>
2022-03-23 10:58:49 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * function that concatenates two strings
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+        char c = "dest";
+        char d = "src\n";
+        char *dest;
+
+                dest = _strncat(c, d);
+        return (dest);
+}
+	
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
