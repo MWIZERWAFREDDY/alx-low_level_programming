a0d9c8102ea23154463213e8a22edf99231b9cb2
MWIZERWAFREDDY <mwizerwafreddy@gmail.com>
2022-03-23 12:49:57 +0000

    comparing two files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * function that copies a string
+ */
+char *_strncpy(char *dest, char *sr
+{
+	char c = "dest";
+        char d = "src";
+        char *dest;
+                dest = _strcmp(c, d);
+        return (dest);
+}
