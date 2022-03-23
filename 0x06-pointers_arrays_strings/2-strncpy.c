ec1749f974416a634b8b5d69707c9031371bb2bc
MWIZERWAFREDDY <mwizerwafreddy@gmail.com>
2022-03-23 11:22:05 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * function that copies a string
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	char c = "dest";
+        char d = "src\n";
+        char *dest;
+                dest = _strncpy(c, d);
+        return (dest);
+}
+
