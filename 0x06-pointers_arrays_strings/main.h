8e1ef2349c91487cb942e7160b6d8680e4db0fc3
FREDDY MWIZERWA <57982668+MWIZERWAFREDDY@users.noreply.github.com>
2022-03-24 12:29:01 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -17,4 +16,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 
-#endif
+
