f089e1064f68bfcb4d79ba3ad194660a3d91e076
MWIZERWAFREDDY <mwizerwafreddy@gmail.com>
2022-03-30 15:16:04 +0000

    print string in recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * a recursive function that prints a string ,followed by a new line
+ */
+void _puts_recursion(char *s);
+{
+	if(*(s) != '\0');
+
+	return (s);
+void main ()
+{
+	char s;
+	printf("%s", _puts_recursion(s);
+}
