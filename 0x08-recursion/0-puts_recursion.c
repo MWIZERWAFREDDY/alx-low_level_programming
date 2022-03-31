cb98ae3bead5ad561bade4ccda736863de7a9cfb
MWIZERWAFREDDY <mwizerwafreddy@gmail.com>
2022-03-31 08:52:10 +0000

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,14 @@
  */
 void _puts_recursion(char *s);
 {
-	if( *(s) != '\0');
-
-	return (s);
-void main ( );
-{
-	_putchar(_puts_recursions(1+s));
+	if (*s != '\0')
+		_putchar(*s);
+		_putchar('\n');
 	return (0);
+
+	void main(void);
+{
+	int b = _puts_recursions(s + 1);
+
+	return (b);
 }
