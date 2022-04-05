7665507199dcd95ad0f51cea6ee5f5c257312fbb
FREDDY MWIZERWA <57982668+MWIZERWAFREDDY@users.noreply.github.com>
2022-04-05 05:18:50 +0200

    Update and rename holberton.h to main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,4 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -9,4 +8,3 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
-#endif
