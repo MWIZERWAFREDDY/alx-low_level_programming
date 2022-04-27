56600bff1c35a55171a475701694bb2d0266f542
FREDDY MWIZERWA <57982668+MWIZERWAFREDDY@users.noreply.github.com>
2022-04-27 02:40:48 +0200

    calculates the sum of all the data in a ln list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,7 +3,6 @@
 /**
  * sum_listint - calculates the sum of all the data in a listint_t list
  * @head: first node in the linked list
- *
  * Return: resulting sum
  */
 int sum_listint(listint_t *head)
